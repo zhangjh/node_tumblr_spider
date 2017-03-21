0b08d818d76dd6bf9bbf1d377e926b20ab1cc7a3
zhangjh <njhxzhangjihong@126.com>
2017-03-21 10:00:55 +0800

    rm useless files

--- bin/main.js
+++ bin/main.js
@@ -1,123 +1,67 @@
-const exec = require('child_process').exec;
-const request = require('request');
-const fs = require('fs');
-const path = require('path');
+/*jshint esversion: 6 */
+let common = require('../lib/common');
+let redis = require('../lib/redis').redis;
+let request = require('request');
 
 //设置默认爬取起始点及爬取深度
-const user = process.argv.slice(2)[0] || "seoul92-choi";
+const user = process.argv.slice(2)[0] || USER;
 const depth = process.argv.slice(2)[1];
 
-let url = API_URL_PRE + user + ".tumblr.com/posts/" + TYPE + "?api_key=" + API_KEY;
-console.log("Starting url: " + url);
+let torrent_url = API_URL_PRE + user + ".tumblr.com/posts/" + TYPE + "?api_key=" + API_KEY + "&reblog_info=true";
+console.log("Starting url: " + torrent_url);
 
-function main(){
-  request(url,(e,r,b) => {
-    if(e){
-      console.error(e);
-      return;
-    }
-    let response = JSON.parse(b).response;
-    let total = response.total_posts;
-    let pages = Math.ceil(total / 20);
-
-    for(let page = 0;page <= pages;page ++){
-      sleep(60 * 1000).then(() => {
-        sendRequest(url,page);
-      });
-    }     
-  });  
-}
-
-function mkLock(){
-  fs.stat("./lock",(err,stats) => {
-    if(err){
-      fs.writeFile("./lock");
-    }
-  });
-}
-
-function releaseLock(){
-  fs.stat("./lock",(err,stats) => {
-    if(!err){
-      fs.unlink("./lock");
-    }
-  });
-}
-
-function sleep(time){
-    return new Promise((resolve,reject) => {
-	fs.stat("./lock",(err,stats) => {
-	  if(err){
-	    resolve();
-	  }else {
-	    let timer = setInterval(() => {
-	      fs.stat("./lock",(err,stats) => {
-	        if(err){
-		  clearInterval(timer);
-		  resolve();
-		}
-	      });
-	    },time);
-	  }
-	});
+function getTotalPage(url) {
+  const p = new Promise((resolve,reject) => {
+    console.log(url);
+    request(url, (e, r, b) => {
+      if (e) {
+        reject(e);
+      } else {
+        let result = common.parseResult(b);
+        resolve(result.pages);
+      }
     });
+  });
+  return p;
 }
 
-function sendRequest(url, page, cb){
-    //console.log("pageUrl: " + url);
-    // 添加并发锁
-    mkLock();
-    request(url + "&offset=" + page, (e,r,b) => {
-        if(e){
-	    console.error(e);
-	    return;
-	}
-	let response = JSON.parse(b).response;
-	let posts = response.posts;
-	let total = response.total_posts;
-	let pages = Math.ceil(total / 20);
-	posts.map(post => {
-	    if(post.video_type === "tumblr"){
-	        console.log(post.video_url);
-	        wget(post.video_url,user);
-	    }
+function doGet(url, page, totalPage) {
+  common.sendRequest(url, page, () => {
+    common.sleep(60 * 1000, () => {
+      if (page++ <= totalPage) {
+        doGet(url, page, totalPage);
+      } else {
+        console.log("One user download all done.");
+        redis.spop("users",function (err, reply) {
+          if(err){
+            console.error(err);
+            redis.quit();
+            process.exit();
+          }else {
+            if(!reply){
+              console.log("All users have been downloaded.");
+              redis.quit();
+              process.exit();
+            }else {
+              let url = API_URL_PRE + reply + ".tumblr.com/posts/" + TYPE + "?api_key=" + API_KEY + "&reblog_info=true";
+              main(url);
+            }
+          }
         });
-	// 下载完成，释放并发锁
-	releaseLock();
-    });
-}
 
-// 创建所有目录
-function mkdirs(dirpath, mode, callback) {
-    fs.stat(dirpath,(err,stats) => {
-      if(!err){
-        callback(dirpath);
-      }else {
-        mkdirs(path.dirname(dirpath),mode,() => {
-	  fs.mkdir(dirpath,mode,callback);
-	});
       }
     });
-};
-
-
-function wget(url,user){
-    const downloadDir = process.cwd() + "/download/" + user + "/";
-    fs.stat(downloadDir,(err,stats) => {
-      if(err){
-        mkdirs(downloadDir,() => {
-	  console.log("创建" + downloadDir);
-	});
-      }
-    });
-    console.log("cmd: wget -P " + downloadDir + " -c " + url);
-    let wget = exec("wget -P " + downloadDir + " -c " + url,function(err,stderr,stdout){
-    	if(err)console.error(err);
-	else {
-	    console.log(stderr);
-	    console.log(stdout);
-	}
-    });
+  });
 }
 
-main();
+function main(url) {
+  getTotalPage(url).then(totalPages => {
+    console.log(totalPages);
+    doGet(url,0,totalPages);
+  }).catch(e => {
+    console.log("Error:" + e);
+  });
+}
+
+// Start from torrent url
+main(torrent_url);

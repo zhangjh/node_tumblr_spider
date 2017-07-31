7da82227d3613d4f52e5e00bf5f58f5c13b996ae
zhangjihong <njhxzhangjihong@126.com>
2017-07-31 21:22:34 +0800

    update

--- conf/config.js
+++ conf/config.js
@@ -1,7 +1,7 @@
 // True to show download progress info
 LOG_MODE = true;
 // The torrent user's name
-USER = "seoul92-choi";
+USER = "mjj1024";
 API_URL_PRE = "https://api.tumblr.com/v2/blog/";
 API_KEY = "rubC6NTRYd6rMMqWIjMFaV213yMtxEGVHYF8TLn6HHfmQoo2VL";
 // Download type: video,photo,audio and etc
